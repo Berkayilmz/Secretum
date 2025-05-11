f3c58a54eaf9843fb7727bd0e9ef7e6106b5121d
Berkay Yılmaz <berkayyilmz01@gmail.com>
2025-05-11 03:31:33 +0300

    env

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,27 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  initializeAuth,
+  getReactNativePersistence
+} from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
+import Constants from 'expo-constants';
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig.extra.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig.extra.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.FIREBASE_APP_ID,
+};
+
+// App'i bir kez initialize et
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+const db = getFirestore(app);
+
+export { app, auth, db };
